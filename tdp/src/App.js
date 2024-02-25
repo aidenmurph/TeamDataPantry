8ccd6a2a3389fff516864c071534730b2d707d64
Matthew Menold <menoldm@flip1.engr.oregonstate.edu>
2024-02-25 14:46:21 -0800

    npx create imported violinicon added HomePage.jsx

---
+++ tdp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePage from './HomePage';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ tdp/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Updated import for React 18
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter> {/* Wrap your App in BrowserRouter */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
