f4f940119ed44d7ab47b205df58b6f2383517268
Jacob Barber <barber.jacobervin@gmail.com>
2024-03-07 17:38:02 -0600

    Added dividers to make nav element more user friendly--this is a temp fix before we style the website

--- frontend/src/components/Navigation.mjs
+++ frontend/src/components/Navigation.mjs
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 function Navigation() {
   return (
     <nav className="global">
-        <Link to="/">Home</Link>
-        <Link to="/composers">Composers</Link>
-        <Link to="/catalogues">Catalogues</Link>
-        <Link to="/compositions">Compositions</Link>
-        <Link to="/movements">Movements</Link>
-        <Link to="/forms">Forms</Link>
-        <Link to="/instruments">Instruments</Link>
-        <Link to="/key-signatures">Key Signatures</Link>
+        <Link to="/">Home</Link> | 
+        <Link to="/composers"> Composers</Link> | 
+        <Link to="/catalogues"> Catalogues</Link> |
+        <Link to="/compositions"> Compositions</Link> | 
+        <Link to="/movements"> Movements </Link> | 
+        <Link to="/forms"> Forms </Link> | 
+        <Link to="/instruments"> Instruments</Link> | 
+        <Link to="/key-signatures"> Key Signatures</Link>
     </nav>
   );
 }
