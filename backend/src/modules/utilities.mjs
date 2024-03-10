6093d14abc2002493ad22bda0183b5f50aef83a1
Jacob Barber <barber.jacobervin@gmail.com>
2024-03-09 18:57:10 -0600

    Updated formatSQL() to remove repeated spaces

--- backend/src/modules/utilities.mjs
+++ backend/src/modules/utilities.mjs
@@ -1,6 +1,6 @@
 // Strips newlines and tabs out of a query that is formatted in source code
 function formatSQL(query) {
-  return query.replace(/[\n\t]/g, ' ');
+  return query.replace(/[\n\t]/g, ' ').replace(/\s+/g, ' ');
 }
 
 export { formatSQL }
