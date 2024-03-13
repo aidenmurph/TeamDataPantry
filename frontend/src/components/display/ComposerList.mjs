47f73c04401d8452bf79b53585705736c5eedab4
Jacob Barber <barber.jacobervin@gmail.com>
2024-03-13 15:17:32 -0500

    Implemented sorting for Composers

--- frontend/src/components/display/ComposerList.mjs
+++ frontend/src/components/display/ComposerList.mjs
@@ -1,29 +1,45 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { SortButton } from '../SortButton.mjs';
 import ComposerRow from './ComposerRow.mjs';
 
-function ComposerList({ composers, onDelete, onEdit }) {
-    return (
-        <table id="composers" className="composers">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Birth Date</th>
-                    <th>Death Date</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {composers.map((composer, i) => 
-                    <ComposerRow 
-                        composer={composer} 
-                        key={i}
-                        onDelete={onDelete}
-                        onEdit={onEdit} 
-                    />)}
-            </tbody>
-        </table>
-    );
+function ComposerList({ composers, activeSortAttribute, onSort, onDelete, onEdit }) {
+  // Use the useNavigate module for redirection
+  const redirect = useNavigate();
+
+  return (
+    <table id="composers" className="composers">
+      <thead>
+        <tr>
+          <th>Name <SortButton attribute={"lastName"} activeSort={activeSortAttribute} onSort={onSort}/></th>
+          <th>Birth Date <SortButton attribute={"birthDate"} activeSort={activeSortAttribute} onSort={onSort}/></th>
+          <th>Death Date <SortButton attribute={"deathDate"} activeSort={activeSortAttribute} onSort={onSort}/></th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {composers.map((composer, i) => 
+          <ComposerRow 
+            key={i}
+            composer={composer} 
+            onDelete={onDelete}
+            onEdit={onEdit} 
+          />)}
+        <tr>
+          <td colSpan={5} style={{ textAlign: "center" }}>
+          <button
+              className="buttonGeneral addButton"
+              type="button"
+              onClick={() => redirect("/add-composer")}
+              id="addComposer"
+            >Add New Composer
+          </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
 
 export default ComposerList;
--- frontend/src/pages/ComposersPage.jsx
+++ frontend/src/pages/ComposersPage.jsx
@@ -3,28 +3,35 @@ import { useNavigate } from 'react-router-dom';
 import ComposerList from '../components/display/ComposerList.mjs';
 import { server_url } from '../config';
 import * as fetchers from '../modules/fetchService.mjs'
+import { sortList } from '../modules/utilities.mjs';
 
 function ComposersPage({ setComposerToEdit }) {
 
   // Use the useNavigate module for redirection
   const redirect = useNavigate();
 
-  // Define state variable for displaying composers
+  // Define state variables for displaying composers
   const [composers, setComposers] = useState([]);
+  const [activeSort, setActiveSort] = useState({});
 
   // RETRIEVE the entire list of composers
   const loadComposers = useCallback(() => {
     fetchers.fetchComposers(setComposers);
   }, []);
 
+  // LOAD all the composers on page startup
+  useEffect(() => {
+    loadComposers();
+  }, [loadComposers]);
+
   // UPDATE a single composer
-  const onEditComposer = async (composer) => {
+  const handleEditComposer = async (composer) => {
     setComposerToEdit(composer);
     redirect('/edit-composer');
   }
 
   // DELETE a single composer
-  const onDeleteComposer = async (id) => {
+  const handleDeleteComposer = async (id) => {
     const response = await fetch(`${server_url}/api/composers/${id}`, { method: 'DELETE'});
     if (response.ok) {
       loadComposers();
@@ -33,10 +40,23 @@ function ComposersPage({ setComposerToEdit }) {
     }
   }
 
-  // LOAD all the composers
+  // Update the active sort and sort the composers
+  const handleSortComposers = (attribute, ascending) => {
+    if (composers.length > 0) {
+      setActiveSort({
+        attribute: attribute,
+        ascending: ascending
+      })
+      setComposers(sortList(composers, attribute, ascending))
+    }
+  }
+  
+  // Maintain the active sort when the composer list is modified (i.e. through deletion)
   useEffect(() => {
-    loadComposers();
-  }, [loadComposers]);
+    if (activeSort.attribute && composers.length > 0) {
+      setComposers(sortList(composers, activeSort.attribute, activeSort.ascending));
+    }
+  }, [composers.length]);
 
   // DISPLAY the composers
   return (
@@ -44,16 +64,12 @@ function ComposersPage({ setComposerToEdit }) {
       <h2>Composers</h2>
       <p>See a live list of the composers in our database. Click the add button below to add a new composer to the collection. Click the edit button to the right of a single composer to modify that entry. Click the delete button to remove that entry.</p>
       <ComposerList 
-          composers={composers} 
-          onEdit={onEditComposer} 
-          onDelete={onDeleteComposer} 
+          composers={composers}
+          onSort={handleSortComposers}
+          activeSortAttribute={activeSort.attribute} 
+          onEdit={handleEditComposer} 
+          onDelete={handleDeleteComposer} 
       />
-      <button
-          className="buttonGeneral addButton"
-          type="addComposer"
-          onClick={() => redirect("/add-composer")}
-          id="addComposer"
-          >Add New Composer</button>
     </>
   );
 }
